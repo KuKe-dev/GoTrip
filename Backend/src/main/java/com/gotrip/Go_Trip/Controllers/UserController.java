934a7144bea08612849647b65d6c85a849aa3b12
KuKe-dev <lucadibene08@gmail.com>
2025-06-03 11:53:03 -0300

    aaAAAAaAaAaAa

--- Backend/src/main/java/com/gotrip/Go_Trip/Controllers/UserController.java
+++ Backend/src/main/java/com/gotrip/Go_Trip/Controllers/UserController.java
@@ -88,11 +88,11 @@ public class UserController {
             String token = jwtUtilities.generateToken(user.getId().toString(), user.getUsername(), user.getEmail());
 
             ResponseCookie cookie = ResponseCookie.from("isLogged", token)
-                .httpOnly(true)
+                .httpOnly(false)
                 .secure(true)
                 .path("/")
                 .maxAge(24 * 60 * 60) // Seconds (24h)
-                .sameSite("None")
+                .sameSite("")
                 .domain("gotripsv.onrender.com")
                 .build();
 
