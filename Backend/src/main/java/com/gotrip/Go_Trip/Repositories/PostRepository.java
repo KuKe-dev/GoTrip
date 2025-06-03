87969a281704ed021f815d53ab6a2c3c5a49ee11
KuKe-dev <lucadibene08@gmail.com>
2025-06-03 16:47:04 -0300

    getPostByUserId cambio

--- Backend/src/main/java/com/gotrip/Go_Trip/Repositories/PostRepository.java
+++ Backend/src/main/java/com/gotrip/Go_Trip/Repositories/PostRepository.java
@@ -18,7 +18,7 @@ public interface PostRepository extends JpaRepository<Post,Long> {
     @Query(value = "SELECT * FROM posts WHERE id = :id", nativeQuery = true)
     Post getPostById(@Param("id") Long id);
 
-    @Query(value = "SELECT * FROM posts WHERE `user-id` = :id", nativeQuery = true)
+    @Query(value = "SELECT * FROM posts WHERE \"user-id\" = :id", nativeQuery = true)
     List<Post> getPostByUserId(@Param("id") Long id);
 
     @Query(value = "SELECT img FROM posts WHERE id = :id", nativeQuery = true)
